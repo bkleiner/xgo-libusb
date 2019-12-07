c0707ad6e5d0f2641eb19ae60f69d40287689928
bkleiner <b.kleiner@str8labs.com>
2019-12-07 16:35:57 +0100

    add osx libusb linker flags

--- run-env.sh
+++ run-env.sh
@@ -16,6 +16,8 @@ case "$PLATFORM" in
         export CC=x86_64-w64-mingw32-gcc-posix CXX=x86_64-w64-mingw32-g++-posix PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/lib/pkgconfig
         ;;
     "darwin")
+        # hack for libusb
+        export CGO_LDFLAGS="-lobjc -Wl,-framework,IOKit -Wl,-framework,CoreFoundation"
         export CC=o64-clang CXX=o64-clang++ PKG_CONFIG_PATH=/usr/x86_64-apple-darwin/lib/pkgconfig
         ;;
 esac
